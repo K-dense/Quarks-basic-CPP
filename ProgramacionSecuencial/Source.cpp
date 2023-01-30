2d90db7c0aae10d4699c4930fffa5efc9cfe0ea0
Cadence <t.r.hillside@gmail.com>
2023-01-30 15:16:35 +0000

    Added spaced between lines for better readability

--- ProgramacionSecuencial/Source.cpp
+++ ProgramacionSecuencial/Source.cpp
@@ -6,10 +6,13 @@ using namespace std;
 int main() {
 	cout << "Please, type a number and press 'Enter' to select an item from the list: ";
 	cout << "\n";
+	cout << "\n";
 	cout << "0. Area of a Square" << endl << "1. Add and Multiply" << endl << "2. Add and Average" << endl << "3. How much does it cost";
+	cout << "\n";
 	cout << "\n";
 
 	int choice;
+	cout << "Select an item: ";
 	cin >> choice;
 
 	switch (choice) {
